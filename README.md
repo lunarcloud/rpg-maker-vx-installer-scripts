bbc5bfd47b0842495ff0f817a1eedd84dbfb8dd4
Samuel Sarette <samuelsarette@linux.com>
2015-09-02 18:44:59 -0400

    Initial commit of the script and resources

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # rpg-vx-ace-deb-creator
 Create wine-dependant debian packages for RPG Maker VX Ace games
+
+## Instructions
+  1. Drop your extracted game into this folder
+  2. Update the game.png
+  3. (Optional) update the company.png
+  4. (Optional) create a license.txt
+  5. Update the information in gameinfo.conf
+  6. Run build-package.sh
+  7. Test and distribute your deb file
---
+++ build-package.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Get Variables
+GAMEFOLDER=$(find ./ -name 'Game.exe' -printf '%h\n' | sort -u | tr -d '\n' | tr -d '\r')
+
+TITLE_UPPER=$(grep 'Title' "$GAMEFOLDER"/Game.ini | cut -d'=' -f 2 | tr -d '\n' | tr -d '\r')
+TITLE_LOWER=$(echo $TITLE_UPPER  | tr '[:upper:]' '[:lower:]')
+TITLE_LOWER_UNDERSCORE=$(echo $TITLE_LOWER  | sed -e 's/ /_/g')
+TITLE_LOWER_DASH=$(echo $TITLE_LOWER  | sed -e 's/ /-/g')
+
+COMPANY_UPPER=$(grep 'Company' ./gameinfo.conf | cut -d'=' -f 2 | tr -d '\n' | tr -d '\r')
+COMPANY_LOWER=$(echo $COMPANY_UPPER  | tr '[:upper:]' '[:lower:]')
+COMPANY_LOWER_UNDERSCORE=$(echo $COMPANY_LOWER  | sed -e 's/ /_/g')
+COMPANY_LOWER_DASH=$(echo $COMPANY_LOWER  | sed -e 's/ /-/g')
+
+VERSION=$(grep 'Version' ./gameinfo.conf | cut -d'=' -f 2 | tr -d '\n' | tr -d '\r')
+DESCRIPTION=$(grep 'Description' ./gameinfo.conf | cut -d'=' -f 2 | tr -d '\n' | tr -d '\r')
+MAINTANER=$(grep 'Maintainer' ./gameinfo.conf | cut -d'=' -f 2 | tr -d '\n' | tr -d '\r')
+HOMEPAGE=$(grep 'Homepage' ./gameinfo.conf | cut -d'=' -f 2 | tr -d '\n' | tr -d '\r')
+
+PACKAGENAME="$COMPANY_LOWER_UNDERSCORE"-"$TITLE_LOWER_DASH"
+DEBIANNAME="$PACKAGENAME"_all
+
+# Create temp control file
+cp control control.temp
+`sed -i "s/Version: \(.*\)/Version: $VERSION/"  ./control.temp`
+`sed -i "s/Description: \(.*\)/Description: $DESCRIPTION/"  ./control.temp`
+`sed -i "s/Maintainer: \(.*\)/Maintainer: $MAINTANER/"  ./control.temp`
+`sed -i "s/Homepage: \(.*\)/Homepage: $HOMEPAGE/"  ./control.temp`
+`sed -i "s/Package: \(.*\)/Package: $PACKAGENAME/"  ./control.temp`
+
+# Create temp desktop file
+cp app.desktop app.desktop.temp
+`sed -i "s/Comment=\(.*\)/Comment=$DESCRIPTION/"  ./app.desktop.temp`
+`sed -i "s/Name=\(.*\)/Name=$TITLE_UPPER/"  ./app.desktop.temp`
+`sed -i "s/Name=\(.*\)/Name=$TITLE_UPPER/"  ./app.desktop.temp`
+`sed -i "s/Exec=\(.*\)/Exec=wine \/opt\/"$PACKAGENAME"\/Game.exe/"  ./app.desktop.temp`
+`sed -i "s/Icon=\(.*\)/Icon=\/opt\/"$PACKAGENAME"\/game.png/"  ./app.desktop.temp`
+
+# Create fakeroot
+mkdir -p "$DEBIANNAME"/DEBIAN
+mkdir -p "$DEBIANNAME"/opt/"$PACKAGENAME"
+mkdir -p "$DEBIANNAME"/usr/share/applications/
+mkdir -p "$DEBIANNAME"/usr/share/pixmaps/
+
+# Copy file into them
+
+cp ./control.temp "$DEBIANNAME"/DEBIAN/control
+cp -r "$GAMEFOLDER"/* "$DEBIANNAME"/opt/"$PACKAGENAME"/
+if [ -f ./license.txt ]; then
+	cp -r ./license.txt "$DEBIANNAME"/opt/"$PACKAGENAME"/LICENSE
+fi
+if [ -f ./company.png ]; then
+	cp ./company.png "$DEBIANNAME"/usr/share/pixmaps/"$COMPANY_LOWER_DASH".png
+fi
+cp ./app.desktop.temp "$DEBIANNAME"/usr/share/applications/"$PACKAGENAME".desktop
+
+# Build the package
+dpkg-deb --build "$DEBIANNAME" "$DEBIANNAME".deb
+
+# Cleanup
+rm -r "$DEBIANNAME"
+
+exit 0
+
+
