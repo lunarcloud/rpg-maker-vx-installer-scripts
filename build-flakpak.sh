d44eb778e7f9490d2b4cb518f876289bc2f0cd62
Samuel Sarette <samuelsarette@linux.com>
2019-06-09 22:45:19 -0400

    flatpak script update #4

--- build-flakpak.sh
+++ build-flakpak.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-echo "script not ready for production."
-exit 9001
-
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [[ ! -e "$CURRENT_DIR"/mkxp-20180121 ]]; then
@@ -30,10 +27,30 @@ if [[ ! -d "$GAMEFOLDER" ]]; then
     exit 31;
 fi
 
+TITLE_UPPER=$(grep 'Title' "$GAMEFOLDER"/Game.ini | cut -d'=' -f 2 | tr -d '\n' | tr -d '\r')
+TITLE_LOWER=$(echo $TITLE_UPPER  | tr '[:upper:]' '[:lower:]')
+TITLE_LOWER_UNDERSCORE=$(echo $TITLE_LOWER  | sed -e 's/ /_/g' | sed -e 's/\.//g')
+TITLE_LOWER_DASH=$(echo $TITLE_LOWER  | sed -e 's/ /-/g' | sed -e 's/\.//g')
+
+COMPANY_UPPER=$(grep 'Company' $DATA_DIR/gameinfo.conf | cut -d'=' -f 2- | tr -d '\n' | tr -d '\r')
+COMPANY_LOWER=$(echo $COMPANY_UPPER  | tr '[:upper:]' '[:lower:]')
+COMPANY_LOWER_UNDERSCORE=$(echo $COMPANY_LOWER  | sed -e 's/ /_/g')
+COMPANY_LOWER_DASH=$(echo $COMPANY_LOWER  | sed -e 's/ /-/g')
+
 ID=$(grep 'Id' $DATA_DIR/gameinfo.conf | cut -d'=' -f 2 | tr -d '\n' | tr -d '\r' | tr -d '[:space:]')
+VERSION=$(grep 'Version' $DATA_DIR/gameinfo.conf | cut -d'=' -f 2 | tr -d '\n' | tr -d '\r')
+SHORT_DESCRIPTION=$(grep 'Description' $DATA_DIR/gameinfo.conf | cut -d'=' -f 2 | tr -d '\n' | tr -d '\r')
+DESCRIPTION=$(sed -n '/Description/,$p' $DATA_DIR/gameinfo.conf | cut -d'=' -f 2- | sed ':a;N;$!ba;s/\n/\\n/g;s/	/ /g')
+MAINTANER=$(grep 'Maintainer' $DATA_DIR/gameinfo.conf | cut -d'=' -f 2 | tr -d '\n' | tr -d '\r')
+HOMEPAGE=$(grep 'Homepage' $DATA_DIR/gameinfo.conf | cut -d'=' -f 2 | tr -d '\n' | tr -d '\r')
+
+PACKAGENAME="$COMPANY_LOWER_DASH"-"$TITLE_LOWER_DASH"
+EXECUTABLENAME="$TITLE_LOWER_DASH"
 
 # TODO create the $ID.json file
 
+echo "TODO"
+exit 9001
 flatpak-builder --bundle-sources --force-clean --repo=repo app $ID.json #--filesystem=~/.$ID
 flatpak build-bundle repo $ID.flatpak $ID
 rm -r repo app
