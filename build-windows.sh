36dea2cceee415ffe50d54e17b3347b3b45a2c62
Samuel Sarette <samuelsarette@linux.com>
2023-09-04 16:06:16 -0400

    moved exe icon updater inside the build-windows

--- build-windows.sh
+++ build-windows.sh
@@ -35,7 +35,50 @@ if [ -f "$DATA_DIR"/installer.ico ]; then
 else
     sed -i "s#define MUI_ICON \"\(.*\)\"#define MUI_ICON \"\$\{NSISDIR\}\\\\Contrib\\\\Graphics\\\\Icons\\\\modern-install.ico\"#"  "$CURRENT_DIR/game.nsi"
 fi
-"$CURRENT_DIR/change-exe-icon.sh" "$DATA_DIR"
+
+
+function changeExeIcon() {
+    if [[ ! -f "$DATA_DIR"/game.ico ]]; then
+        return
+    fi
+
+    # Get Variables
+    EXECUTABLE=$(find "$DATA_DIR" ! -path "*_i386*" ! -path "*_amd64/*" ! -path "*.app*" -name 'Game.exe' -printf '%p')
+
+    if [[ ! -e "$EXECUTABLE" ]]; then
+        echo "No game executable found inside \"$DATA_DIR\""
+        return
+    fi
+
+    # Ensure the tool exists
+    if [[ ! -d "$CURRENT_DIR"/resources/tool/resource_hacker ]]; then
+        if [[ ! -f "$CURRENT_DIR"/resources/tool/resource_hacker.zip ]]; then
+            # download the archive
+            wget http://www.angusj.com/resourcehacker/resource_hacker.zip -P "$CURRENT_DIR"/tool
+        fi
+        mkdir -p "$CURRENT_DIR"/resources/tool/
+        unzip "$CURRENT_DIR"/resources/tool/resource_hacker.zip -d "$CURRENT_DIR"/resources/tool/resource_hacker
+        if [[ ! -d "$CURRENT_DIR"/resources/tool/resource_hacker ]]; then
+            echo "resource hacker tool does not exist"
+            return
+        fi
+        # remove the archive
+        rm "$CURRENT_DIR"/resources/tool/resource_hacker.zip
+    fi
+
+    #if [[ ! -e "$EXECUTABLE.bak" ]]; then
+        #cp "$EXECUTABLE" "$EXECUTABLE.bak"
+    #fi
+
+    cp "$DATA_DIR/game.ico" "$DATA_DIR/1.ico"
+
+    wine "$CURRENT_DIR"/resources/tool/resource_hacker/ResourceHacker.exe -open "$EXECUTABLE" -save "$EXECUTABLE" -log "$CURRENT_DIR"/resources/tool/resource_hacker/resourcehacker.log -resource "$DATA_DIR/1.ico" -action addoverwrite -mask ICONGROUP,1,
+
+    rm "$DATA_DIR/1.ico"
+}
+
+# Update the executable's icon
+changeExeIcon
 
 cd "$CURRENT_DIR"
 makensis "$CURRENT_DIR/game.nsi"
