66bcaa84a707c1fa13da4e35909303cdc770600f
Samuel Sarette <samuelsarette@linux.com>
2023-09-05 10:28:35 -0400

    fix datamanager example

--- example files/DataManager.rb
+++ example files/DataManager.rb
@@ -5,7 +5,7 @@
   def self.save_folder
     directory_name = ''
     if OS.windows?
-      directory_name = Dir.home + "/Application Data/My Game"
+      directory_name = Dir.home + "/AppData/My Game"
     elsif OS.linux?
       directory_name = Dir.home + "/.local/share/lowercasecompany-lowercasegame"
     elsif OS.mac?
